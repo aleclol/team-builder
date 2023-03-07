0ccd1c246c0e6ca6dc805287ac981c8d4f9f62ec
Alec Barber <alecbarber23@gmail.com>
2023-03-06 20:02:01 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import './App.css'
 
 
 export default function App () {
+  const [data, setData] = useState({
+    firstName: "Rick",
+    lastName: "Sanchez",
+    age: 67
+  })
+
+const onChange = (evt) => {
+  setData({...data, [evt.target.name]: 'value'})
+}
 
 
 return (
-  <div>
-    <h1>Form Fun!</h1>
-    <form>
-      <label>
-        Text:
-        <input type="text" />
-      </label>
-    </form>
+  <div className="kingClass">
+    <div className="container">
+      <h2>New Team Member</h2>
+      <form>
+        <label>
+          First Name:&nbsp;
+          <input type="text" />
+        </label>
+        <br />
+        <label>
+          Last Name:&nbsp;
+          <input type="text" />
+        </label>
+        <br />
+        <label>
+          Age:&nbsp;
+          <input type="number" />
+        </label>
+      </form>
+    </div>
+    <div className="container">
+      <h2>Team Members</h2>
+    </div>
   </div>
 )
 
