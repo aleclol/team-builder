653abcebfe256db957887cd76032165823acae2d
Alec Barber <alecbarber23@gmail.com>
2023-03-06 19:31:20 -0500

    set up basic react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+export default function App () {
+
+
+return (
+  <div>
+    <h1>Form Fun!</h1>
+    <form>
+      <label>
+        Text:
+        <input type="text" />
+      </label>
+    </form>
+  </div>
+)
+
+}
